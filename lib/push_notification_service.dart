c6baf750e12ab744c7254cacc15642fe0dfe3d82
Handeme <hanannadhif@gmail.com>
2024-11-12 11:27:52 +0700

    Update Chatlist & env

--- lib/push_notification_service.dart
+++ lib/push_notification_service.dart
@@ -1,39 +1,52 @@
 import 'dart:convert';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:googleapis_auth/auth_io.dart' as auth;
 import 'package:http/http.dart' as http;
 
 class PushNotificationService {
   // Fungsi untuk mendapatkan Access Token menggunakan akun layanan Firebase
   Future<String> getAccessToken() async {
-    final serviceAccountJson = {"YOUR_SERVICE_ACCOUNT_JSON"};
+    await dotenv.load(); // Memuat file .env
 
-    // Scope yang diperlukan untuk mengakses Firebase Messaging
+    final serviceAccountJson = {
+      "type": "service_account",
+      "project_id": dotenv.env['GOOGLE_CLOUD_PROJECT_ID'],
+      "private_key_id": dotenv.env['GOOGLE_CLOUD_PRIVATE_KEY_ID'],
+      "private_key": dotenv.env['GOOGLE_CLOUD_PRIVATE_KEY'],
+      "client_email": dotenv.env['GOOGLE_CLOUD_CLIENT_EMAIL'],
+      "client_id": dotenv.env['GOOGLE_CLOUD_CLIENT_ID'],
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url":
+          "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url":
+          "https://www.googleapis.com/robot/v1/metadata/x509/chatify-app%40chatify-a9f62.iam.gserviceaccount.com",
+      "universe_domain": "googleapis.com"
+    };
+
     List<String> scopes = [
-      "https://www.googleapis.com/auth/firebase.database",
       "https://www.googleapis.com/auth/firebase.messaging",
+      "https://www.googleapis.com/auth/firebase.database"
     ];
 
-    // Membuat client dengan akun layanan
     final client = await auth.clientViaServiceAccount(
       auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
       scopes,
     );
 
-    // Mendapatkan token akses
     final credentials = await client.credentials;
 
     return credentials.accessToken.data;
   }
 
-  // Fungsi untuk mengirim notifikasi push ke perangkat
+  // Fungsi untuk mengirim notifikasi push
   Future<void> sendNotification(
       String deviceToken, String title, String body) async {
     final serverAccessTokenKey = await getAccessToken();
     final endpoint =
         'https://fcm.googleapis.com/v1/projects/chatify-a9f62/messages:send';
 
-    // Data pesan yang akan dikirimkan
     final message = {
       'message': {
         'token': deviceToken,
@@ -47,7 +60,6 @@ class PushNotificationService {
       }
     };
 
-    // Mengirimkan pesan ke FCM
     final response = await http.post(
       Uri.parse(endpoint),
       headers: {
@@ -57,7 +69,6 @@ class PushNotificationService {
       body: jsonEncode(message),
     );
 
-    // Mengecek respons
     if (response.statusCode == 200) {
       print("Notifikasi berhasil dikirim.");
     } else {
